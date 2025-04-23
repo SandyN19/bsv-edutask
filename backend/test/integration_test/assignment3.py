a4c76aa9bd0b087b09d0a6cb6615d758655d04e1
sunny <nerdbuster123@gmail.com>
2025-04-23 15:49:02 +0200

    Started on assignment 3, NotImplementedError currently

--- backend/test/integration_test/assignment3.py
+++ backend/test/integration_test/assignment3.py
@@ -1,10 +1,24 @@
 import pytest
-from unittest.mock import MagicMock
-from src.controllers.usercontroller import UserController
+from pymongo.errors import WriteError
+import pytest
+import mongomock
+from unittest.mock import patch
 from src.util.dao import DAO
 
 @pytest.fixture
-def setup_controller():
-    mock_dao = MagicMock(spec=DAO)
-    controller = UserController(dao=mock_dao)
-    return controller, mock_dao
+def mocked_dao():
+    with patch('pymongo.MongoClient', new=mongomock.MongoClient):
+        dao = DAO('video')
+        yield dao
+        dao.drop()
+
+def test_create_valid(mocked_dao):
+    data = { 
+        "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley", 
+        "description": "Totally not rick roll" 
+        }
+    result = mocked_dao.create(data)
+    assert result["url"] == data["url"]
+    assert result["description"] == data["description"]
+    assert "_id" in result
+
