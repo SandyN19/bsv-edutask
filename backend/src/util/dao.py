709fb151dcb14e250103e12b7a578c3ba89c2ca1
sunny <nerdbuster123@gmail.com>
2025-04-26 15:55:47 +0200

    More line breaks in docstring

--- backend/src/util/dao.py
+++ backend/src/util/dao.py
@@ -44,7 +44,8 @@ class DAO:
         The creation of a new document must comply to the corresponding validator, which defines the data structure of the collection. 
         In particular, the validator has to make sure that: 
         (1) the data for the new object contains all required properties, 
-        (2) every property complies to the bson data type constraint (see https://www.mongodb.com/docs/manual/reference/bson-types/, though we currently only consider Strings and Booleans), 
+        (2) every property complies to the bson data type constraint (see https://www.mongodb.com/docs/manual/reference/bson-types/, 
+        though we currently only consider Strings and Booleans), 
         (3) and the values of a property flagged with 'uniqueItems' are unique among all documents of the collection.
 
         parameters:
