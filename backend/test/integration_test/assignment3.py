a1fbe1de7e623fd24257776aea78581c854df536
sunny <nerdbuster123@gmail.com>
2025-04-26 15:56:54 +0200

    Assignment 3 Integration tests

--- backend/test/integration_test/assignment3.py
+++ backend/test/integration_test/assignment3.py
@@ -1,24 +1,47 @@
 import pytest
-from pymongo.errors import WriteError
-import pytest
-import mongomock
 from unittest.mock import patch
 from src.util.dao import DAO
+from pymongo.errors import WriteError
 
 @pytest.fixture
 def mocked_dao():
-    with patch('pymongo.MongoClient', new=mongomock.MongoClient):
-        dao = DAO('video')
-        yield dao
-        dao.drop()
+    dao = DAO("todo")
+    yield dao
+    dao.collection.delete_one({"description": "test"})
 
 def test_create_valid(mocked_dao):
-    data = { 
-        "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley", 
-        "description": "Totally not rick roll" 
-        }
+    data = {
+        "description": "test",
+        "done": False,
+    }
     result = mocked_dao.create(data)
-    assert result["url"] == data["url"]
     assert result["description"] == data["description"]
+    assert result["done"] == data["done"]
     assert "_id" in result
 
+def test_create_invalid(mocked_dao):
+    data = {
+        "description": "test",
+        "done": "not_a_boolean",
+    }
+
+    with pytest.raises(WriteError):
+        mocked_dao.create(data)
+
+def test_create_none(mocked_dao):
+    with pytest.raises(TypeError):
+        mocked_dao.create(None)
+
+def test_create_empty(mocked_dao):
+    with pytest.raises(WriteError):
+        mocked_dao.create({})
+
+def test_create_duplicate(mocked_dao):
+    data = {
+        "description": "duplicate_test",
+        "done": False,
+    }
+    mocked_dao.create(data)
+
+    with pytest.raises(WriteError):
+        mocked_dao.create(data)
