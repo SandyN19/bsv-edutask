899ce89433abe10c2279f93161e96e8f509ae8cd
sunny <nerdbuster123@gmail.com>
2025-05-05 15:24:52 +0200

    Labb 2 completed

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -1,54 +1,93 @@
-describe('Creating a new task', () => {
+describe('Labb 2', () => {
+  // define variables that we need on multiple occasions
+  let uid // user id
+  let name // name of the user (firstName + ' ' + lastName)
+  let email // email of the user
+
   before(function () {
-      // create a fabricated user from a fixture
-      cy.fixture('user.json')
-        .then((user) => {
-          cy.request({
-              method: 'POST',
-              url: 'http://localhost:5000/users/create',
-              form: true,
-              body: user
-          }).then((response) => {
-              cy.log(response.body)
-              this.uid = response.body._id.$oid
-              this.name = user.firstName + ' ' + user.lastName
-              this.email = user.email
+    // create a fabricated user from a fixture
+    cy.fixture('user.json')
+      .then((user) => {
+        cy.request({
+          method: 'POST',
+          url: 'http://localhost:5000/users/create',
+          form: true,
+          body: user
+        }).then((response) => {
+          cy.log(response.body)
+          uid = response.body._id.$oid
+          name = user.firstName + ' ' + user.lastName
+          email = user.email
+        })
+      })
+  })
 
-              const taskData = new URLSearchParams();
-              taskData.append('title', 'I love Shrek');
-              taskData.append('userid', this.uid);
-              taskData.append('url', 'https://www.youtube.com/watch?v=em9lziI07M4&ab_channel=Movieclips');
+  beforeEach(function () {
+    cy.visit('http://localhost:3000')
+    cy.contains('div', 'Login').find('input[type=text]').first().type('mon.doe@gmail.com');
+    cy.get('form').submit();
+    cy.wait(1000); // wait 1 second for page to load
+  });
 
-            cy.request({
-              method: 'POST',
-              url: 'http://localhost:5000/tasks/create',
-              form: true,
-              body: taskData
-              });
-            });
-          });
-        });
+  it('R8UC1-A: Should allow creating a todo item when description is populated', () => { 
+    cy.get('input[type="text"]')
+    .first()
+    .type('I love Shrek1');
+    cy.get('input[type="text"]').eq(1).type('https://www.youtube.com/watch?v=em9lziI07M4');
+    cy.get('form').submit();
+    cy.wait(500);
+    cy.contains('div', 'I love Shrek1').should('exist').click({ force: true });
+    cy.wait(200);
+    cy.get('input[placeholder=\"Add a new todo item\"]').type('show my friends Shrek', { force: true});
+    cy.get('input[type="submit"]').eq(1).click({ force: true });
+  });
+
+  it('R8UC1-B: Add button should be disabled when description is empty', () => {
+    cy.get('div').contains('I love Shrek1').should('exist').click({ force: true });
+    cy.wait(1000);
+    cy.get('input[type="submit"]').eq(1).should('be.disabled');
+  });
+
+  it('R8UC2-A: Should strike through text', () => {
+    cy.contains('div', 'I love Shrek1').should('exist').click({ force: true });
+    cy.wait(200);
+    cy.get('li.todo-item').eq(1).within(() => {
+      cy.get('span.checker').click({ force: true }); // toggle on
+      cy.get('span.checker')
+        .should('have.class', 'checked')
+        .and('not.have.class', 'unchecked');
+      cy.get('span.editable')
+        .should('have.css', 'text-decoration-line', 'line-through');
     });
-
-    beforeEach(function () {
-      cy.visit('http://localhost:3000')
-      cy.contains('div', 'Email Address').find('input[type=text]').type(this.email);
-      cy.get('form').submit();
-      cy.contains('div', 'I love Shrek').click();
+  });
+  
+  it('R8UC2-B: Should un-strike through text', () => {
+    cy.contains('div', 'I love Shrek1').should('exist').click({ force: true });
+    cy.wait(200);
+    cy.get('li.todo-item').eq(1).within(() => {
+      cy.get('span.checker').click({ force: true }); // toggle off
+      cy.get('span.checker').should('have.class', 'unchecked').and('not.have.class', 'checked');
+      cy.get('span.editable').should('not.have.css', 'text-decoration-line', 'line-through');
     });
-    
-    it('R8UC1: Should allow creating a todo item when description is populated', () => { 
-      cy.get('input[placeholder="Add a new todo item"]')
-      .type('Show Shrek to my friends');
-      cy.get('form').submit();
-      cy.contains('div', 'Show Shrek to my friends').should('exist');
+  });
+  
+  it('R8UC3: Should allow deleting a todo item', () => {
+    cy.contains('div', 'I love Shrek1').should('exist').click({ force: true});
+    cy.wait(200);
+    cy.get('li.todo-item').eq(1).within(() => {
+      cy.get('span.remover').click({ force: true});
+    });
+    cy.wait(200);
+    cy.get('li.todo-item').eq(1).should('not.exist');
+  })
+
+  after(function () {
+    // clean up by deleting the user from the database
+    cy.request({
+      method: 'DELETE',
+      url: `http://localhost:5000/users/${uid}`
+    }).then((response) => {
+      cy.log(response.body)
     })
-
-  //   after(function () {
-  //     cy.request({
-  //       method: 'DELETE',
-  //       url: `http://localhost:5000/users/${this.uid}`
-  //     }).then((response) => {
-  //       cy.log(response.body)
-  //     })
-  // })
+  })
+})
